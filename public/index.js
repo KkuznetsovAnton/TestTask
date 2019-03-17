7857e526b3c683827116dcc15e4a499dee190b91
Anton <kuz.aanton@bk.ru>
2019-03-18 02:04:51 +0300

    delete mock-data.js , add dates

--- public/index.js
+++ public/index.js
@@ -1,56 +1,58 @@
-var fromServer = [
-  {
-    id: 0,
-    date: "09.03",
-    day: "Monday",
-    films: [
-      {
-        film: "Film-1",
-        seanceTime: ["12:00", "16:00", "20:00"]
-      },
-      {
-        film: "Film-2",
-        seanceTime: ["10:00", "14:00", "18:00"]
-      }
-    ]
-  },
-  {
-    id: 1,
-    date: "10.03",
-    day: "Tuesday",
-    films: [
-      {
-        film: "Film-2",
-        seanceTime: ["12:00", "16:00", "20:00"]
-      },
-      {
-        film: "Film-1",
-        seanceTime: ["10:00", "14:00", "18:00"]
-      }
-    ]
-  }
-];
-
-var fromServer2 = {
-  "09.03": {
+var mockData = {
+  "18.03": {
     day: "Monday",
     films: {
       "king-kong": ["10:00", "14:00", "18:00"],
-      avatar: ["12:00", "16:00", "20:00"]
+      "avatar": ["12:00", "16:00", "20:00"]
     }
   },
-  "10.03": {
+  "19.03": {
     day: "Tuesday",
     films: {
-      avengers: ["10:00", "14:00", "18:00"],
-      interstellar: ["12:00", "16:00", "20:00"]
+      "avengers": ["10:00", "14:00", "18:00"],
+      "interstellar": ["12:00", "16:00", "20:00"]
     }
-  }
+  },
+  "20.03": {
+    day: "Wednesday",
+    films: {
+      "avengers": ["10:00", "14:00", "18:00"],
+      "interstellar": ["12:00", "16:00", "20:00"]
+    }
+  },
+  "21.03": {
+    day: "Thursday",
+    films: {
+      "avengers": ["10:00", "14:00", "18:00"],
+      "interstellar": ["12:00", "16:00", "20:00"]
+    }
+  },
+  "22.03": {
+    day: "Friday",
+    films: {
+      "avengers": ["10:00", "14:00", "18:00"],
+      "interstellar": ["12:00", "16:00", "20:00"]
+    }
+  },
+  "23.03": {
+    day: "Saturday",
+    films: {
+      "avengers": ["10:00", "14:00", "18:00"],
+      "interstellar": ["12:00", "16:00", "20:00"]
+    }
+  },
+  "24.03": {
+    day: "Sunday",
+    films: {
+      "avengers": ["10:00", "14:00", "18:00"],
+      "interstellar": ["12:00", "16:00", "20:00"]
+    }
+  },
 };
 
 var seanceSelect = document.querySelector("#dateSeance");
 
-Object.keys(fromServer2).forEach(function(item) {
+Object.keys(mockData).forEach(function(item) {
   var option = document.createElement("option");
   option.value = item;
   option.text = item;
@@ -60,7 +62,7 @@ var filmSelect = document.querySelector("#film");
 
 seanceSelect.onchange = function() {
   filmSelect.innerHTML = "";
-  Object.keys(fromServer2[seanceSelect.value].films).forEach(function(item) {
+  Object.keys(mockData[seanceSelect.value].films).forEach(function(item) {
     var option = document.createElement("option");
     option.value = item;
     option.text = item;
@@ -71,7 +73,8 @@ seanceSelect.onchange = function() {
 var timeSelect = document.querySelector("#timeSeance");
 
 filmSelect.onchange = function() {
-  fromServer2[seanceSelect.value].films[filmSelect.value].forEach(function(
+  timeSelect.innerHTML = "";
+  mockData[seanceSelect.value].films[filmSelect.value].forEach(function(
     item
   ) {
     var option = document.createElement("option");
