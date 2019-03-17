00198623fe363881a383d118c7366d36259fed5e
Anton <kuz.aanton@bk.ru>
2019-03-18 02:51:39 +0300

    add css

--- public/index.js
+++ public/index.js
@@ -3,51 +3,51 @@ var mockData = {
     day: "Monday",
     films: {
       "king-kong": ["10:00", "14:00", "18:00"],
-      "avatar": ["12:00", "16:00", "20:00"]
+      avatar: ["12:00", "16:00", "20:00"]
     }
   },
   "19.03": {
     day: "Tuesday",
     films: {
-      "avengers": ["10:00", "14:00", "18:00"],
-      "interstellar": ["12:00", "16:00", "20:00"]
+      avengers: ["10:00", "14:00", "18:00"],
+      interstellar: ["12:00", "16:00", "20:00"]
     }
   },
   "20.03": {
     day: "Wednesday",
     films: {
-      "avengers": ["10:00", "14:00", "18:00"],
-      "interstellar": ["12:00", "16:00", "20:00"]
+      avengers: ["10:00", "14:00", "18:00"],
+      interstellar: ["12:00", "16:00", "20:00"]
     }
   },
   "21.03": {
     day: "Thursday",
     films: {
-      "avengers": ["10:00", "14:00", "18:00"],
-      "interstellar": ["12:00", "16:00", "20:00"]
+      avengers: ["10:00", "14:00", "18:00"],
+      interstellar: ["12:00", "16:00", "20:00"]
     }
   },
   "22.03": {
     day: "Friday",
     films: {
-      "avengers": ["10:00", "14:00", "18:00"],
-      "interstellar": ["12:00", "16:00", "20:00"]
+      avengers: ["10:00", "14:00", "18:00"],
+      interstellar: ["12:00", "16:00", "20:00"]
     }
   },
   "23.03": {
     day: "Saturday",
     films: {
-      "avengers": ["10:00", "14:00", "18:00"],
-      "interstellar": ["12:00", "16:00", "20:00"]
+      avengers: ["10:00", "14:00", "18:00"],
+      interstellar: ["12:00", "16:00", "20:00"]
     }
   },
   "24.03": {
     day: "Sunday",
     films: {
-      "avengers": ["10:00", "14:00", "18:00"],
-      "interstellar": ["12:00", "16:00", "20:00"]
+      avengers: ["10:00", "14:00", "18:00"],
+      interstellar: ["12:00", "16:00", "20:00"]
     }
-  },
+  }
 };
 
 var seanceSelect = document.querySelector("#dateSeance");
@@ -74,9 +74,7 @@ var timeSelect = document.querySelector("#timeSeance");
 
 filmSelect.onchange = function() {
   timeSelect.innerHTML = "";
-  mockData[seanceSelect.value].films[filmSelect.value].forEach(function(
-    item
-  ) {
+  mockData[seanceSelect.value].films[filmSelect.value].forEach(function(item) {
     var option = document.createElement("option");
     option.value = item;
     option.text = item;
