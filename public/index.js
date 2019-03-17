573352fdac1d138eeee2939c67f81cbff6cb0476
Anton <kuz.aanton@bk.ru>
2019-03-17 23:25:10 +0300

    filmSelect and timeSelect implimented

--- public/index.js
+++ public/index.js
@@ -31,18 +31,52 @@ var fromServer = [
   }
 ];
 
+var fromServer2 = {
+  "09.03": {
+    day: "Monday",
+    films: {
+      "king-kong": ["10:00", "14:00", "18:00"],
+      avatar: ["12:00", "16:00", "20:00"]
+    }
+  },
+  "10.03": {
+    day: "Tuesday",
+    films: {
+      avengers: ["10:00", "14:00", "18:00"],
+      interstellar: ["12:00", "16:00", "20:00"]
+    }
+  }
+};
 
-var seanceSelect = document.querySelector("#dataSeance");
+var seanceSelect = document.querySelector("#dateSeance");
 
-fromServer.forEach(function(item) {
+Object.keys(fromServer2).forEach(function(item) {
   var option = document.createElement("option");
-  option.value = item.date;
-  option.text = item.date;
+  option.value = item;
+  option.text = item;
   seanceSelect.add(option);
 });
-var currentDay;
+var filmSelect = document.querySelector("#film");
 
-seanceSelect.onchange = function(e){
-    console.log(this.value);
-    currentDay = this.value;
-}
+seanceSelect.onchange = function() {
+  filmSelect.innerHTML = "";
+  Object.keys(fromServer2[seanceSelect.value].films).forEach(function(item) {
+    var option = document.createElement("option");
+    option.value = item;
+    option.text = item;
+    filmSelect.add(option);
+  });
+};
+
+var timeSelect = document.querySelector("#timeSeance");
+
+filmSelect.onchange = function() {
+  fromServer2[seanceSelect.value].films[filmSelect.value].forEach(function(
+    item
+  ) {
+    var option = document.createElement("option");
+    option.value = item;
+    option.text = item;
+    timeSelect.add(option);
+  });
+};
